e8d3609e2162bd7f4011eda942c2afc8de5fa1e8
g g <gabedotgutierrez@gmail.com>
2024-10-14 15:10:58 -0400

    li mapped

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,102 @@ import './App.css'
 function App() {
   const [count, setCount] = useState(0)
 
+// Create a new state variable named team and set the initial state to an empty array [].
+// Create a new state variable named money and set the initial state to 100.
+// Create a new state variable named zombieFighters and set the initial state to the following array of objects:
+
+  const [team,setTeam] = useState([])
+  const [money,setMoney] = useState(0)
+  const [zombieFighters, setZombieFighters] = useState(
+    [
+      {
+        name: 'Survivor',
+        price: 12,
+        strength: 6,
+        agility: 4,
+        img: 'https://via.placeholder.com/150/92c952',
+      },
+      {
+        name: 'Scavenger',
+        price: 10,
+        strength: 5,
+        agility: 5,
+        img: 'https://via.placeholder.com/150/771796',
+      },
+      {
+        name: 'Shadow',
+        price: 18,
+        strength: 7,
+        agility: 8,
+        img: 'https://via.placeholder.com/150/24f355',
+      },
+      {
+        name: 'Tracker',
+        price: 14,
+        strength: 7,
+        agility: 6,
+        img: 'https://via.placeholder.com/150/d32776',
+      },
+      {
+        name: 'Sharpshooter',
+        price: 20,
+        strength: 6,
+        agility: 8,
+        img: 'https://via.placeholder.com/150/1ee8a4',
+      },
+      {
+        name: 'Medic',
+        price: 15,
+        strength: 5,
+        agility: 7,
+        img: 'https://via.placeholder.com/150/66b7d2',
+      },
+      {
+        name: 'Engineer',
+        price: 16,
+        strength: 6,
+        agility: 5,
+        img: 'https://via.placeholder.com/150/56acb2',
+      },
+      {
+        name: 'Brawler',
+        price: 11,
+        strength: 8,
+        agility: 3,
+        img: 'https://via.placeholder.com/150/8985dc',
+      },
+      {
+        name: 'Infiltrator',
+        price: 17,
+        strength: 5,
+        agility: 9,
+        img: 'https://via.placeholder.com/150/392537',
+      },
+      {
+        name: 'Leader',
+        price: 22,
+        strength: 7,
+        agility: 6,
+        img: 'https://via.placeholder.com/150/602b9e',
+      }
+  ]
+)
+
   return (
-    <h1>Hello world!</h1>
+    // Display the list of zombieFighters by mapping the array into the UI of App.jsx. (We’ve provided some helpful CSS assuming you use a ul and lis)
+    
+    <ul>
+      {zombieFighters.map((zf)=>
+        <>
+        <li><img src={zf.img} alt={zf.name}></img>
+        <li>name: {zf.name}</li>
+        <li>price:{zf.price}</li>
+        <li>strength: {zf.strength}</li>
+        <li>agility: {zf.agility}</li>
+        </li>
+        </>
+      )}
+    </ul>
   );
 }
 
